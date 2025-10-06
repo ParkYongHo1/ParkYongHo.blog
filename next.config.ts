f5f8945e602b22f50dfb0727452b510fdd5b8314
ParkYongHo1 <qkaxhf8823@naver.com>
2025-10-07 02:24:45 +0900

    Fix | 배포 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-// next.config.ts
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -11,6 +11,17 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
 
